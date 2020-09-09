52ee674ed383bbbea339887cf9060121b1d01d1b
Garlic Fiend <suzannaakins@gmail.com>
2020-09-09 13:38:28 -0700

    add query parameter to href link to second html page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -58,7 +58,7 @@ var displayRepos = function (repos, searchTerm) {
         //create a container for each repo
         var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
-        repoEl.setAttribute("href", "./single-repo.html");
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
         //create a span element to hold repository name
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
